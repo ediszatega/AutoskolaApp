16a5d7d79c2dac5c0b45c441facf5b49fdd1591b
Edis Zatega <edis.zatega@edu.fit.ba>
2022-12-05 22:44:58 +0100

    41 - Add add user

--- frontend/src/app/components/user/user.component.ts
+++ frontend/src/app/components/user/user.component.ts
@@ -10,6 +10,9 @@ import { ApiConfig } from '../../../services/api-config';
 export class UserComponent implements OnInit {
   allUsers: any;
   search: string = '';
+  selectedUser: any;
+  allCities: any;
+  cityId: number = 0;
 
   constructor(private httpClient: HttpClient) {}
 
@@ -23,6 +26,10 @@ export class UserComponent implements OnInit {
       .subscribe((x: any) => {
         this.allUsers = x;
       });
+
+    this.httpClient.get(ApiConfig.base_url + '/City/GetAll').subscribe((x) => {
+      this.allCities = x;
+    });
   }
 
   getUsers() {
@@ -38,5 +45,35 @@ export class UserComponent implements OnInit {
       .subscribe((x: any) => {
         this.fetchData();
       });
+  }
+
+  addUser() {
+    this.selectedUser = {
+      id: 0,
+      firstName: '',
+      lastName: '',
+      username: '',
+      password: '',
+      cityId: this.cityId,
+    };
+  }
+
+  save() {
+    this.selectedUser.cityId = this.cityId;
+    console.log(this.selectedUser);
+    console.log(this.cityId);
+    if (this.selectedUser.id == 0) {
+      this.httpClient
+        .post(ApiConfig.base_url + '/User/Add', this.selectedUser)
+        .subscribe((x: any) => {
+          this.fetchData();
+          this.addUser();
+        });
+    }
+  }
+
+  cancel() {
+    this.selectedUser = null;
+    this.fetchData();
   }
 }
